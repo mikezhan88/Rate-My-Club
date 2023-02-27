51e99a3b268cbdf5b63c0fec7957eaa7738eba02
Nitya Simhadri <nityasimhadri@Kirans-MBP.lan>
2023-02-26 16:54:27 -0800

    added a dropdown menu for register and linked adding a club to register page. still need to finish tweaking drop down menu

--- rate-my-club/src/App.js
+++ rate-my-club/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import LandingPage from './components/LandingPage/LandingPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <React.Fragment>
       <Routes>
         <Route exact path="/" element= {<LandingPage/>} />
+        <Route exact path="/register" element= {<RegisterPage/>} />
       </Routes>
     </React.Fragment>
   );
--- rate-my-club/src/components/NavBar/NavBar.js
+++ rate-my-club/src/components/NavBar/NavBar.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import React, {useState, useEffect, useRef} from 'react';
 
 export default function NavBar() {
+
+  const [open, setOpen] = useState(false);
   return (
     <div className='navBar'>
       <Link to='/'>
@@ -13,10 +15,34 @@ export default function NavBar() {
         <Link to='/login'>
           <button className='navBar-button'>Login</button>
         </Link>
-        <Link to='/register'>
-        <button className='navBar-button'>Register</button>
-        </Link>
-      </span>
+       
+        <div className='navbar-menu-container'>
+              <div className='navbar-menu-trigger'onClick={()=>{setOpen(!open)}}>
+                
+                  <button className='navBar-button'>Register</button>
+                 
+                </div>
+            
+                <div className= {`navbar-dropdown-menu ${open? 'active' : 'inactive'}`}>
+                    <ul>
+                        <Link to='/register' style={{ textDecoration: 'none', color: 'black' }}>
+                          <DropdownItem text = {"Create a New Club"}/>
+                        </Link>
+                        <DropdownItem text = {"Create a User Profile"}/>
+                    </ul>
+                </div>
+            
+          </div>
+          </span>
+      
     </div>
   )
 }
+
+function DropdownItem(props){
+    return(
+        <li className= 'navbar-dropdownItem'>
+            <a>{props.text}</a>
+        </li>
+    );
+  }
