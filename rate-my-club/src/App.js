0444fab23ec68c6588fa535275d0b8ee9ea9fc4d
Sae Tsunawaki <saetsunawaki@gmail.com>
2023-02-26 21:28:19 -0800

    Finish User Login Page

--- rate-my-club/src/App.js
+++ rate-my-club/src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import LandingPage from './components/LandingPage/LandingPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
-import NavBar from './components/NavBar/NavBar';
+import LoginPage from './components/Login-Page/LoginPage';
 
 function App() {
   return (
@@ -11,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element= {<LandingPage/>} />
         <Route exact path="/register" element= {<RegisterPage/>} />
+        <Route exact path="/login" element= {<LoginPage/>} />
       </Routes>
     </React.Fragment>
   );
