47850c622a9dde7a334051c4c8c16fb5673535aa
Sae Tsunawaki <saetsunawaki@gmail.com>
2023-02-27 18:03:29 -0800

    Add dropdown in register page

---
+++ rate-my-club/src/components/RegisterPage/Dropdown.js
@@ -0,0 +1,98 @@
+import React, {useState, useEffect} from 'react';
+
+export default function Dropdown({ placeholder, options, isMulti }) {
+
+    const [showMenu, setShowMenu] = useState(false);
+    const [selectedValue, setSelectedValue] = useState(isMulti ? [] : null);
+
+    useEffect(() => {
+        const handler = () => setShowMenu(false);
+        window.addEventListener("click", handler);
+        return () => {
+            window.removeEventListener("click", handler);
+        };
+    });
+
+    const handleInputClick = (e) => {
+        e.stopPropagation();
+        setShowMenu(!showMenu);
+    };
+
+    const getDisplay = () => {
+        if (!selectedValue || selectedValue.length === 0) {
+            return placeholder;
+        }
+        if (isMulti) {
+            return (
+                <div className='dropdown-tags'>
+                    {selectedValue.map((option) => (
+                        <div key={option.value} className='dropdown-tag-item'>
+                            { option.label }
+                            <span onClick={(e) => onTagRemove(e, option)} className='dropdown-tag-close'>
+                                <img src={require("../images/close-icon.png")} className='close-icon' alt="close-icon" />
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            );
+        }
+        return selectedValue.label;
+    };
+
+    const onItemClick = (option) => {
+        let newValue;
+        if (isMulti) {
+            if (selectedValue.findIndex((o) => o.value === option.value) >= 0) {
+                newValue = removeOption(option);
+            }
+            else {
+                newValue = [...selectedValue, option];
+            }
+        }
+        else {
+            newValue = option;
+        }
+        setSelectedValue(newValue);
+    };
+
+    const isSelected = (option) => {
+        if (isMulti) {
+            return selectedValue.filter((o) => o.value === option.value).length > 0;
+        }
+        if (!selectedValue) {
+            return false;
+        }
+        return selectedValue.value === option.value;
+    }
+
+    const removeOption = (option) => {
+        return selectedValue.filter((o) => o.value !== option.value);
+    };
+
+    const onTagRemove = (e, option) => {
+        e.stopPropagation();
+        setSelectedValue(removeOption(option));
+    };
+
+    return (
+        <div className='dropdown-container'>
+            <div onClick={ handleInputClick } className='dropdown-input'>
+                <div className='dropdown-selected-value'> { getDisplay() } </div>
+                <div className='dropdown-tools'>
+                    <div className='dropdown-tool'>
+                        <img src={require("../images/dropdown-logo.png")} className='dropdown-logo' alt="logo" />
+                    </div>
+                </div>
+            </div>
+            { showMenu && (
+                <div className='dropdown-menu'>
+                { options.map((option) => (
+                    <div onClick={ () => onItemClick(option) } key={ option.value } className={`dropdown-item ${isSelected(option) && "selected"}`}>
+                        { option.label }
+                    </div>
+                ))}
+                </div>
+            )}
+        </div>
+    )
+}
--- rate-my-club/src/components/RegisterUserPage/RegisterUserPage.js
+++ rate-my-club/src/components/RegisterUserPage/RegisterUserPage.js
@@ -6,23 +6,18 @@ import Form from 'react-bootstrap/Form';
 export default function RegisterPage() {
     return (
           <React.Fragment>
-
            <NavBar/>
-
            <div className='register-page'>
                 <img src={require("../images/logo.png")} className='register-logo' alt="logo" />
                 <div className='register-largetext'>Welcome to Rate My Club!</div>
                 <div className='register-user-page-backdrop'>
                     <input type='text' className='club-register-input' placeholder='Full Name *' ></input>
-   
-
                     <input type='text' className='club-register-input' placeholder='Nick Name' ></input>
                     <input type='text' className='club-register-input' placeholder='Email Address *' ></input>
                     <input type='text' className='club-register-input' placeholder='Password *' ></input>
                     <button type='button' className='register-button'>Register</button>
                 </div>
            </div>   
-
          </React.Fragment>
   )
 }
