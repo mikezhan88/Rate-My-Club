777c67e9b0a3820c771f7b892ed6a6746a2e608f
DrewLetvin <32301845+DrewLetvin@users.noreply.github.com>
2023-02-27 16:40:05 -0800

    Create Flask Server
    create flask server and reorganize into frontend and backend

---
+++ rate-my-club/backend/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import datetime
+
+x = datetime.datetime.now()
+
+# Initializing flask app
+app = Flask(__name__)
+
+
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+
+    # Returning an api for showing in  reactjs
+    return {
+        "Date":x,
+        }
+
+
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
--- rate-my-club/frontend/src/App.js
+++ rate-my-club/frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import {Route, Routes} from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/Login-Page/LoginPage';
@@ -8,6 +8,26 @@ import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
+    // usestate for setting a javascript
+    // object for storing and using data
+    const [data, setdata] = useState({
+        date: "",
+    });
+
+    // Using useEffect for single rendering
+    useEffect(() => {
+        // Using fetch to fetch the api from
+        // flask server it will be redirected to proxy
+        fetch("/data").then((res) =>
+            res.json().then((data) => {
+                // Setting a data from api
+                setdata({
+                    date: data.Date,
+                });
+            })
+        );
+    }, []);
+
   return (
     <React.Fragment>
       <Routes>
@@ -16,7 +36,11 @@ function App() {
         <Route exact path="/login" element= {<LoginPage/>} />
         <Route exact path="/registeruser" element= {<RegisterUserPage/>} />
       </Routes>
+
+        
     </React.Fragment>
+
+
   );
 }
 
