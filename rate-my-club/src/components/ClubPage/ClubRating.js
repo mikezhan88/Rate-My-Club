92990bc9ee7b713bdd9dcab8d34947950a4beaa1
Sae Tsunawaki <saetsunawaki@gmail.com>
2023-03-07 23:55:58 -0800

    Create components for club page

---
+++ rate-my-club/src/components/ClubPage/ClubRating.js
@@ -0,0 +1,26 @@
+import { BsStarFill, BsStar} from "react-icons/bs";
+
+export const AverageRating = props => {
+    var average = props.club.reviews.reduce((a, b) => a + b.rating, 0)/(props.club.reviews.length);
+    average = average.toFixed(1);
+    return (
+        <div className='average-rating'>{average}</div>
+    );
+}
+
+export const AverageRatingStars = props => {
+    var average = props.club.reviews.reduce((a, b) => a + b.rating, 0)/(props.club.reviews.length);
+    average = Math.trunc(average)
+    return (
+        <div className='stars'>
+            {
+                new Array(average).fill(null).map(() => (
+                    <BsStarFill size={20} className='star'></BsStarFill>
+                ))}
+                {new Array(5-average).fill(null).map(() => (
+                    <BsStar size={20} className='star'></BsStar>
+                ))
+            }
+        </div>
+    );
+}
