621ea426a43bba85155981ebbe734c5249869ad1
Drew Letvin <drewletvin@hotmail.com>
2023-03-12 17:27:51 -0700

    remove history

--- rate-my-club/src/index.js
+++ rate-my-club/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
-import { createBrowserHistory } from "history";
+//import { createBrowserHistory } from "history";
 
-const history = createBrowserHistory();
+//const history = createBrowserHistory();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter history={history}>
+  <BrowserRouter >
     <App />
   </BrowserRouter>
 );
