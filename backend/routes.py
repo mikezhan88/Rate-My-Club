757554ba2003dcbbd8f81d95627bcf979691294d
DrewLetvin <32301845+DrewLetvin@users.noreply.github.com>
2023-03-17 16:47:31 -0700

    add testing and sample test to backend

--- backend/routes.py
+++ backend/routes.py
@@ -10,6 +10,13 @@ reviews_router = APIRouter()
 clubs_router = APIRouter()
 users_router = APIRouter()
 
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(3) == 5
+
+
 #POST /review
 @reviews_router.post("/", response_description="Create a new review", status_code=status.HTTP_201_CREATED, response_model=Review)
 def create_review(request: Request, review: Review = Body(...)):
