97e1227d2e5e9dcd74f67dd611d2e9082406f401
DrewLetvin <32301845+DrewLetvin@users.noreply.github.com>
2023-03-18 00:44:36 -0700

    Post and Delete Club Tests

--- backend/test_routes.py
+++ backend/test_routes.py
@@ -32,3 +32,37 @@ def test_delete_review():
     post_response = 200
 
     assert response.status_code == post_response
+
+
+def test_post_club():
+    data = {
+        "_id": "066de609-1111-0000-1111-32537c7f1pop",
+        "name": "Pytest club",
+        "description": "Cool Space Club",
+        "email": "rocket@gmail.com",
+        "profile_picture": "boop",
+        "tags": [],
+        "clubWebsite": "www.ucladsp.com",
+        "clubSize": "50-100 ppl",
+        "commitmentLevel": "2-5 hours",
+        "updates": []
+    }
+
+    url = "http://localhost:8000/clubs/"
+    response = requests.post(url, json=data)
+
+    post_response = 201
+
+    assert response.status_code == post_response
+
+
+
+def test_delete_club():
+    data = {"_id": "066de609-1111-0000-1111-32537c7f1pop"}
+
+    url = "http://localhost:8000/clubs/066de609-1111-0000-1111-32537c7f1pop"
+    response = requests.delete(url, json=data)
+
+    post_response = 200
+
+    assert response.status_code == post_response
