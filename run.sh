f8435a4b5feef0b8a2f1877d369af45cbb27a7fe
Pranav Maddali <60276842+pranav-maddali@users.noreply.github.com>
2023-03-18 23:47:36 -0700

    bash build script DONE

--- run.sh
+++ run.sh
@@ -1,2 +1,17 @@
-cd rate-my-club & npm install & npm start &
-gnome-terminal & cd ../backend & pip install -r requirements.txt & python -m uvicorn server:app 
+#! /bin/bash
+
+cleanup() {
+    pkill -P $$
+}
+
+for sig in INT QUIT HUP TERM; do
+    trap "
+        cleanup
+        trap - $sig EXIT
+        kill -s $sig "'"$$"' "$sig"
+done
+trap cleanup EXIT
+
+cd backend;
+pip install -r requirements.txt;
+python -m uvicorn server:app & (cd ../rate-my-club; npm install; npm start;);
